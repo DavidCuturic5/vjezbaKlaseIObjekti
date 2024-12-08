9624af7a9074981cf77987936b2a1512bb096bac
DavidCuturic5 <david.cuturic5@gmail.com>
2024-12-08 18:57:51 +0100

    Add files via upload

---
+++ vjezbaKlaseIObjekti/vjezbaKlaseIObjekti/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezbaKlaseIObjekti
+{
+
+    class Zrakoplov
+    {
+        string naziv;
+        double snagaMotora;
+        int dosegLeta;
+
+        private string Naziv { get => naziv; set => naziv = value; }
+        private double SnagaMotora { get => snagaMotora; set => snagaMotora = value; }
+        private int DosegLeta { get => dosegLeta; set => dosegLeta = value; }
+
+        
+        public Zrakoplov() { }
+
+        public Zrakoplov(string naziv, double snagaMotora, int dosegLeta)
+        {
+            this.naziv = naziv;
+            this.snagaMotora = snagaMotora;
+            this.dosegLeta = dosegLeta;
+            
+        }
+        
+
+        public override string ToString()
+        {
+            string ispis = "Naziv: " + this.naziv + ", " + "Snaga motora: " + this.snagaMotora + "kW" + ", " + "Doseg Leta: " + this.dosegLeta + "km";
+
+            return ispis;
+        } 
+    
+       
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Zrakoplov x380 = new Zrakoplov("Airbus", 2000, 6000);
+            Zrakoplov C162 = new Zrakoplov("Cessna Skycatcher", 75, 870);
+            Zrakoplov PC21 = new Zrakoplov("Pilatus", 1200, 1333);
+
+            Console.WriteLine(x380.ToString());
+            Console.WriteLine(C162.ToString());
+            Console.WriteLine(PC21.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ vjezbaKlaseIObjekti/vjezbaKlaseIObjekti/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("vjezbaKlaseIObjekti")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("vjezbaKlaseIObjekti")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d63adb27-5b9d-45c8-a350-aa5586ba2c4b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
